9e9b13e59f6b30049c68ee402a2adbe570ad3157
oxyblade <oxyblade78@gmail.com>
2022-11-16 23:59:35 +0100

    advance time

--- Timer/Timer/Form1.Designer.cs
+++ Timer/Timer/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Timer
 {
-    partial class Form1
+    partial class window
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -30,12 +30,101 @@ namespace Timer
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(window));
+            this.restart = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.timerlabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.setting = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(231, 232);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(75, 23);
+            this.restart.TabIndex = 0;
+            this.restart.Text = "restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // pause
+            // 
+            this.pause.Location = new System.Drawing.Point(150, 232);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(75, 23);
+            this.pause.TabIndex = 1;
+            this.pause.Text = "pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(192, 261);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 2;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timerlabel
+            // 
+            this.timerlabel.AutoSize = true;
+            this.timerlabel.Font = new System.Drawing.Font("Consolas", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerlabel.ForeColor = System.Drawing.Color.Red;
+            this.timerlabel.Location = new System.Drawing.Point(126, 90);
+            this.timerlabel.Name = "timerlabel";
+            this.timerlabel.Size = new System.Drawing.Size(207, 75);
+            this.timerlabel.TabIndex = 3;
+            this.timerlabel.Text = "00:00";
+            this.timerlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.timerlabel.Click += new System.EventHandler(this.timerlabel_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // setting
+            // 
+            this.setting.Image = ((System.Drawing.Image)(resources.GetObject("setting.Image")));
+            this.setting.Location = new System.Drawing.Point(424, 12);
+            this.setting.Name = "setting";
+            this.setting.Size = new System.Drawing.Size(26, 23);
+            this.setting.TabIndex = 4;
+            this.setting.UseVisualStyleBackColor = true;
+            this.setting.Click += new System.EventHandler(this.setting_Click);
+            // 
+            // window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(462, 296);
+            this.Controls.Add(this.setting);
+            this.Controls.Add(this.timerlabel);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.restart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "window";
+            this.Text = "timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label timerlabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button setting;
     }
 }
 
--- Timer/Timer/Form1.cs
+++ Timer/Timer/Form1.cs
@@ -10,11 +10,54 @@ using System.Windows.Forms;
 
 namespace Timer
 {
-    public partial class Form1 : Form
+    public partial class window : Form
     {
-        public Form1()
+        public window()
         {
             InitializeComponent();
+        }
+
+        //default time 1min
+        int timeleft = 60;
+
+        private void timerlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            timeleft = 60;
+            timer.Stop();
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeleft > 0)
+            {
+                timeleft--;
+                timerlabel.Text = "00:" + timeleft;
+            }
+
+            else
+            {
+                timerlabel.Text = "Finish";
+            }
+        }
+
+        private void setting_Click(object sender, EventArgs e)
+        {
+            //open new window
         }
     }
 }
